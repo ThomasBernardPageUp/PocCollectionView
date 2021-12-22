e79f7259d195494ee62ef2abfc8b09a8ef63bb44
Thomas BERNARD <tbernard@pageup.fr>
2021-12-22 11:46:00 +0100

    Create pages and navigation

--- PocCollectionView/PocCollectionView/App.xaml.cs
+++ PocCollectionView/PocCollectionView/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PocCollectionView.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace PocCollectionView
         {
             InitializeComponent();
             Sharpnado.CollectionView.Initializer.Initialize(true, false);
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
---
+++ PocCollectionView/PocCollectionView/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PocCollectionView.ViewModels
+{
+    public class BaseViewModel
+    {
+        public INavigation NavigationService { get; set; }
+
+
+        public BaseViewModel(INavigation navigation)
+        {
+            NavigationService = navigation;
+        }
+    }
+}
