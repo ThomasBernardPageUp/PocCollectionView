876bba7af9be171f8f468c268c51765a2e0b40f6
Thomas BERNARD <tbernard@pageup.fr>
2021-12-22 13:26:17 +0100

    create model and factory

---
+++ PocCollectionView.Models/UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocCollectionView.Models
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Picture { get; set; }
+        public string Email { get; set;}
+        public string Phone { get; set;}
+
+        public UserEntity(int id, string firstname, string picture, string email, string phone)
+        {
+            Id = id;
+            Firstname = firstname;
+            Picture = picture;
+            Email = email;
+            Phone = phone;
+        }
+    }
+}
---
+++ PocCollectionView.Models/UserFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PocCollectionView.Models
+{
+    public class UserFactory
+    {
+        private static List<string> _names = new List<string>() { "Thomas", "Axel", "Sébastien", "Charlie", "Georges", "Théo", "Kilian", "Samuel", "Anthony", "Hugo"};
+        public static IEnumerable<UserEntity> GenerateUser(int numberOfUser = 100)
+        {
+            List<UserEntity> users = new List<UserEntity>();
+            var random = new Random();
+
+            for (int i = 0; i < numberOfUser; i++)
+            {
+                var user = new UserEntity(i, _names[random.Next(0, _names.Count - 1)], "https://picsum.photos/400/300", "a.b@gmail.com", "06 07 29 01 78");
+                users.Add(user);
+            }
+
+
+            return users;
+        }
+    }
+}
