bfb6a56e7da0da51d9f74b32e0152264082d3ea2
Thomas BERNARD <tbernard@pageup.fr>
2021-12-23 09:29:35 +0100

    Use observable to default list

--- PocCollectionView/PocCollectionView/ViewModels/DefaultCollectionViewModel.cs
+++ PocCollectionView/PocCollectionView/ViewModels/DefaultCollectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PocCollectionView.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -9,10 +10,11 @@ namespace PocCollectionView.ViewModels
 {
     public class DefaultCollectionViewModel : BaseViewModel
     {
-        public IEnumerable<UserEntity> Users => UserFactory.GenerateUser(500);
+        public ObservableCollection<UserEntity> Users { get; set; }
         public ICommand UserTappedCommand { get; set; }
         public DefaultCollectionViewModel(INavigation navigation) : base(navigation)
         {
+            Users = new ObservableCollection<UserEntity>(UserFactory.GenerateUser());
             UserTappedCommand = new Command(DisplayUser);
         }
 
